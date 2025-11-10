ccf96cae085595788bb530ba88fd8af58d20db53
JoaovPazn <joaovitor223344joao@gmail.com>
2025-11-09 22:31:55 -0300

    Insert a Bunch of new content and bugfixing.

--- Server.js
+++ Server.js
@@ -12,25 +12,35 @@ app.engine("handlebars", exphbs.engine({ defaultLayout: false }));
 app.set("view engine", "handlebars");
 
 app.listen(port, () => {
-  console.log(`Servidor em execução: http://localhost:${port}`);
+    console.log(`Servidor em execução: http://localhost:${port}`);
 });
 
 let users = [
-  { id: 1, nome: "DstoneDev", senha: "Waguri123Teto", idade: 17 },
-  { id: 2, nome: "FortieDev", senha: "johnFortune69", idade: 16 },
-  { id: 3, nome: "CrooslDev", senha: "TwitterUser99", idade: 17 },
+    { id: 1, nome: "DstoneDev", senha: "Waguri123Teto", idade: 17, pfp: "pfp0" },
+    { id: 2, nome: "FortieDev", senha: "johnFortune69", idade: 16, pfp: "pfp0" },
+    { id: 3, nome: "CrooslDev", senha: "TwitterUser99", idade: 17, pfp: "pfp0" },
 ];
 
 app.get("/", (req, res) => {
-  const { nome } = req.query;
-  res.render("homePage", { nome: nome });
+    const { nome, pfp } = req.query;
+    res.render("homePage", { nome: nome, pfp: pfp });
+});
+
+app.get("/disconnect", (req, res) => {
+    res.render("homePage");
 });
 
 app.get("/cadastro", (req, res) => res.render("cadastroPage"));
 
 app.post("/cadastro/novo", (req, res) => {
-  const { nome, senha, idade } = req.body;
-  const id = users.length + 1;
-  users.push({ id, nome, senha, idade: parseInt(idade) });
-  res.redirect("/?nome=" + users[id - 1].nome);
+    const { nome, senha, idade, pfp } = req.body;
+    const id = users.length + 1;
+    users.push({ id, nome, senha, idade: parseInt(idade), pfp });
+    res.redirect(`/?nome=${users[id - 1].nome}&pfp=${users[id - 1].pfp}`);
+});
+
+app.get("/user", (req, res) => {
+const { nome, pfp } = req.query;
+res.render("UserPage", { nome: nome, pfp: pfp })
+
 });
